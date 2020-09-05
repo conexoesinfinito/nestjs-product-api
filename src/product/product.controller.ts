f9ef9de5319e7932d78b3a032a8533b7db88fdc0
Horácio Pedro <46960527+horacio-pedro@users.noreply.github.com>
2020-09-05 17:19:54 +0100

    Product interface

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Res, HttpStatus, Body, Get, Param, NotFoundException, Delete, Query, Put, createParamDecorator } from '@nestjs/common';
+import { Controller, Post, Res, HttpStatus, Body, Get, Param, NotFoundException, Delete, Query, Put } from '@nestjs/common';
 import { Response } from 'express'
 import { CreateProductDTO } from './dto/product.dto'
 import { ProductService } from './product.service'
@@ -10,7 +10,7 @@ export class ProductController {
   constructor(private productService: ProductService) {}
 
   @Post('/create')
-  async createPost(@Res() res: Response, @Body() createProductDTO: CreateProductDTO): Promise<Response<any>> {
+  async createPost(@Res() res: Response, @Body() createProductDTO: CreateProductDTO): Promise<Response<Product>> {
     const product = await this.productService.createProduct(createProductDTO)
     return res.status(HttpStatus.OK).json({
       message: 'received',
@@ -27,7 +27,7 @@ export class ProductController {
   }
 
   @Get('/:productID')
-  async getProduct(@Res() res: Response, @Param('productID') productID: string): Promise<Response<any>> {
+  async getProduct(@Res() res: Response, @Param('productID') productID: string): Promise<Response<Product>> {
     const product = await this.productService.getProduct(productID)
     if(!product) throw new NotFoundException('Product does not exists')
     return res.status(HttpStatus.OK).json({
@@ -36,7 +36,7 @@ export class ProductController {
   }
 
   @Delete('/delete')
-  async deleteProduct(@Res() res: Response, @Query('productID') productID: string): Promise<Response<any>> {
+  async deleteProduct(@Res() res: Response, @Query('productID') productID: string): Promise<Response<Product>> {
     const productDeleted = await this.productService.deleteProduct(productID)
     if(!productDeleted) throw new NotFoundException('Product does not exists')
     return res.status(HttpStatus.OK).json({
